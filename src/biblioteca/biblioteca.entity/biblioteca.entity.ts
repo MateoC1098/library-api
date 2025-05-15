8e93e66a7c7cccf588685188b769fd20be39038d
Mateo_Caceres <m.caceresa23@uniandes.edu.co>
2025-05-15 13:09:05 -0500

    Creación entidad biblioteca

---
+++ src/biblioteca/biblioteca.entity/biblioteca.entity.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class BibliotecaEntity {
+@PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  nombre: string;
+
+  @Column()
+  direccion: string;
+
+  @Column()
+  ciudad: string;
+
+  @Column()
+  horarioAtencion: string;
+}
