d68dc8ca10e7d1b82092bd12ca99ae975447733e
Mateo_Caceres <m.caceresa23@uniandes.edu.co>
2025-05-15 19:51:07 -0500

    Configuración de TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BibliotecaModule } from './biblioteca/biblioteca.module';
 import { LibroModule } from './libro/libro.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BibliotecaEntity } from './biblioteca/biblioteca.entity/biblioteca.entity';
+import { LibroEntity } from './libro/libro.entity/libro.entity';
 
 @Module({
-  imports: [BibliotecaModule, LibroModule],
+  imports: [BibliotecaModule, LibroModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'db_library',
+      entities: [BibliotecaEntity, LibroEntity],
+      dropSchema: true,
+      synchronize: true
+    }),
+    BibliotecaModule,
+    LibroModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
