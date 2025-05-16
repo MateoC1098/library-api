c8512e0094159b852a5a15c7f19bb39856f253f1
Mateo_Caceres <m.caceresa23@uniandes.edu.co>
2025-05-15 20:37:30 -0500

    Implementación lógica de Biblioteca

--- src/biblioteca/biblioteca.module.ts
+++ src/biblioteca/biblioteca.module.ts
@@ -1,7 +1,12 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BibliotecaEntity } from './biblioteca.entity/biblioteca.entity';
 import { BibliotecaService } from './biblioteca.service';
 
 @Module({
-  providers: [BibliotecaService]
+  imports: [TypeOrmModule.forFeature([BibliotecaEntity])],
+  providers: [BibliotecaService],
+  exports: [BibliotecaService]
 })
 export class BibliotecaModule {}
